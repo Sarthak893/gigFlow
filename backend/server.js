dae3bae940aa7e1d1dcd60a7855971542f65b819
Sarthak893 <kumarsarthak741@gmail.com>
2026-01-12 13:10:25 +0530

    initialized backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// backend/server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('GigFlow Backend Running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
