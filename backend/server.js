b5304fcd8a873dad18d3c3d07ae826775b29089a
Sarthak893 <kumarsarthak741@gmail.com>
2026-01-13 23:54:04 +0530

    debugging errors in live links

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,25 @@ dotenv.config();
 
 
 const app = express();
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://gig-flow-theta-eight.vercel.app'
-];
+app.use(cors({
+  origin: [
+    'http://localhost:5173',           
+    'https://gig-flow-theta-eight.vercel.app' 
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow server-to-server or Postman
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS not allowed'));
-      }
-    },
-    credentials: true
-  })
-);
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 
 app.use(express.json());
